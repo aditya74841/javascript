fc60f59423af2abcaeb0123ffca663eab92b70c2
Aditya Ranjan <94691391+aditya74841@users.noreply.github.com>
2024-04-18 19:14:11 +0000

    Learn Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)  // We are using not browser so it will not work in nodejs
+
+
+console.log(3
+    +
+    3
+) // Code readability should be high , This is Not the Good Practice (although it will work)
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// String 
+// Number => 2 to power 23
+// BigInt
+// Boolean
+// null => standalone Value  (this means value is empty )
+// undefined  (undefined means value is not defined and this is totally different form null)
+// symbol => mostly use for unique
+
+
+//  onject 
+
+console.log(typeof undefined)  // undefined
+
+
+console.log(typeof null)  // object
