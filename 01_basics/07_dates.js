781db55024d8717f8ebbc2be0b15ef8ff2cfb2b2
Aditya Ranjan <94691391+aditya74841@users.noreply.github.com>
2024-04-21 07:09:00 +0000

    Learned Dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,38 @@
+// Dates
+
+
+let myDate = new Date()
+
+// console.log(myDate);
+
+// console.log(myDate.getDate());
+
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getHours());
+// console.log(myDate.getMilliseconds());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getMonth());
+// console.log(myDate.getTime());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+
+
+// let myCreatedDate = new Date(2024, 3, 21) 
+// console.log(myCreatedDate.toDateString()); //Sun Apr 21 2024
+
+
+
+
+// let myCreatedDate = new Date("01-14-2024") 
+// console.log(myCreatedDate.toDateString()); //Sun Apr 21 2024
+
+
+let myTimeStamp = Date.now()
+
+
+console.log(myTimeStamp) // 1713640062301
