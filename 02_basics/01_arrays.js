276ce4c445432593bd951471cd80cbde9cf865ee
Aditya Ranjan <94691391+aditya74841@users.noreply.github.com>
2024-04-22 17:34:42 +0000

    Learned Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+// array
+
+
+// const adi = ["Aditya", "Ranjan", "Shubham", "Pandey"]
+// const myArr = [1, 2, "Three", true, { name: "Aditya", age: 12 }]
+
+// console.log(myArr[4].age);
+
+
+// const newArray = new Array(1, 2, 3, 4)   // Used To create New Array
+// console.log(newArray);
+
+// newArray.push(12)  // To Push new values
+// console.log(newArray);
+
+// newArray.pop(4)  // To Pop from index  values
+// console.log(newArray);
+
+// newArray.pop()  // To Pop from back
+// console.log(newArray);
+
+// newArray.unshift(9)
+
+// console.log(newArray.includes(9)); //true means yes false means No
+// console.log(newArray.indexOf(9));  // -1 is not present
+
+
+// const myArr = newArray
+
+// console.log(myArr);
+// console.log(newArray);
+
+
+// slice, splice
+
+// console.log("A", myArr)
+// const myn1 = newArray.slice(1, 3);
+
+// console.log(myn1);
+// console.log("B", myArr)
+
+// // Splice will Manupluate the original Array 
+
+// const myn2 = newArray.splice(1, 3);
+// console.log(myn2);
+// console.log("A", myArr)
+
+
