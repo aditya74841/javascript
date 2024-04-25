d17d2ed04def2b766178fa1deb4a1b8e9dcb15b0
Aditya Ranjan <94691391+aditya74841@users.noreply.github.com>
2024-04-25 20:56:24 +0000

    Learned For Loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,52 @@
+// for
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+
+//     console.log(element);
+
+// }
+
+
+
+
+// let a = 0
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         a += 1;
+//         console.log(`Inner loop value ${j}`)
+//         console.log(i + '*' + j + ' = ' + i * j);
+
+//     }
+// }
+// console.log(a);
+
+
+
+// Learn Break
+
+// for (let index = 0; index <=10;  index++) {
+//     if(index==5){
+//         console.log(`5 detected`);
+//         break;
+//     }
+//  console.log(`vlaue of Index is ${index}`);   
+
+
+// }
+
+
+
+// Learn Continue
+
+for (let index = 0; index <=10;  index++) {
+    if(index==5){
+        console.log(`5 detected`);
+        continue;
+    }
+ console.log(`vlaue of Index is ${index}`);   
+
+
+}
